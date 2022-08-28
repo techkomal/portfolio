975252fa1d8fdb905218852df52888f2434199c8
Komal Gautam <104885038+techkomal@users.noreply.github.com>
2022-08-29 00:20:17 +0530

    add files via upload

---
+++ index.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Porfoilio</title>
+    <link rel="shortcut icon" type="image/jpg" href="Favicon_Image_Location"/>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+    
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+        
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.4.0/mdb.min.css" rel="stylesheet"/>
+
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.4.0/mdb.min.js"></script>
+</head>
+<body class="txt">
+    <div class="container-fluid">   
+        <header>
+            <!-- Background image -->
+            <div
+                id="intro-example"
+                class="p-5 bg-image bg"
+                style="background-image: url('img/IMG-20220421-WA0013.jpg');"
+            >
+                <div class="mask" style="background-color: rgba(0, 0, 0, 0.7);">
+                    <div class="d-flex justify-content-right align-items-right h-100">
+                        <div class="text-white">
+                            <button
+                                class="navbar-toggler"
+                                type="button"
+                                data-mdb-toggle="collapse"
+                                data-mdb-target="#navbarExample01"
+                                aria-controls="navbarExample01"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >KOMAL</button>
+                            <a
+                                class="btn btn-outline-light btn-lg m-2"
+                                href="#"
+                                role="button"
+                                rel="nofollow"
+                                target="_blank"
+                            >Home</a>
+                            <a
+                                class="btn btn-outline-light btn-lg m-2"
+                                href="#"
+                                target="_blank"
+                                role="button"
+                            >About us</a>
+                            <a
+                                class="btn btn-outline-light btn-lg m-2"
+                                href="#"
+                                target="_blank"
+                                role="button"
+                            >Projects</a>
+                            <a
+                                class="btn btn-outline-light btn-lg m-2"
+                                href="#"
+                                role="button"
+                                rel="nofollow"
+                                target="_blank"
+                            >Acheivements</a>
+                            <a
+                                class="btn btn-outline-light btn-lg m-2"
+                                href="https://mdbootstrap.com/docs/standard/"
+                                target="_blank"
+                                role="button"
+                            >Porfolio</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Background image -->
+        </header>
+    </div>
+    <!-- About -->
+    <div class="container">
+        <div class="row d-flex p-2 bd-highlight">
+            <h3 class="display-4 head3">About me</h3>
+        <div>   
+    </div>
+    <div class="container"> 
+        <div class="col-sm-12">
+            <div class="card w-100">
+                <div class="card-body">
+                    <p class="card-text"> My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.With supporting text below as a natural lead-in to additional content.</p>
+                    <button class="btn btn-primary">Read More</button>
+                </div>
+            </div>       
+        </div>
+    </div>
+    <!-- Experiance -->
+    <div class="container">
+        <div class="row d-flex p-2 bd-highlight">
+            <h3 class="display-4 head3">Skills</h3>
+        </div>
+    </div>
+    <div class="container">
+            <div class="col-sm-12">
+                <div class="card w-100">
+                    <div class="card-body">
+                        <div class="col-md-6">
+                            <!-- <h4 class="card-title"> <strong> Skills </strong></h4> -->
+                            <p class="card-text">In recent years, it has actually been rather effectively revealed that artficial knowledge systems can adequately create game content of a number of various types: textures, plants, degrees, maps, animals and so forth.</p>
+                            <p class="card-text">There is currently extreme passion in r & d in procedural material generation within both academia as the video game sector, as responses are sought to technical obstacles such as just how to develop even more replayable video games and just how to reduce game development costs. Don't hesitate to try HTML Web Page Gaming Design.</p>
+                        
+                        </div>
+                        <div class="col-md-6">
+                            <h4 class="card-title"> <strong> HTML </strong></h4>
+                            <p class="card-text">In recent years, it has actually been rather effectively revealed that artficial.</p>
+                        
+                            <h4 class="card-title"> <strong> CSS </strong></h4>
+                            <p class="card-text">In recent years, it has actually been rather effectively revealed that artficial.</p>
+                        
+                            <h4 class="card-title"> <strong> PHP </strong></h4>
+                            <p class="card-text">In recent years, it has actually been rather effectively revealed that artficial.</p>
+                        
+                            <h4 class="card-title"> <strong> javascript </strong></h4>
+                            <p class="card-text">In recent years, it has actually been rather effectively revealed that artficial.</p>
+                        
+                            <h4 class="card-title"> <strong> Jquery </strong></h4>
+                            <p class="card-text">In recent years, it has actually been rather effectively revealed that artficial.</p>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+        
+    </div>
+
+    <div class="container">
+        <div class="row d-flex p-2 bd-highlight">
+            <h3 class="display-4 head3">Acheivments</h3>
+        </div>
+        <div class="col-sm-12">
+                <div class="card w-100">
+                    <div class="card-body">
+                        <p class="card-text"> My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.My Religion is very simple, that is kindness. My Religion is very simple, that is kindness.With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>       
+        </div>
+    </div>
+    <div class="container">
+        <div class="row d-flex p-2 bd-highlight">
+            <h3 class="display-4 head3">Projects</h3>
+        </div>
+    </div>
+    <div class="container">
+            <div class="col-sm-4">
+                <div class="card w-80">
+                    <div class="card-body text-center">
+                        <h4 class="card-title"> <strong> TIC TAK TOE </strong></h4>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card w-80">
+                <div class="card-body text-center">
+                    <h4 class="card-title"> <strong>Healthcare</strong> </h4>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card w-80">
+                <div class="card-body text-center">
+                    <h4 class="card-title"> <strong>Belmont Doors</strong> </h4>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+        
+    </div>
+    <div class="container">
+        <div class="row d-flex p-2 bd-highlight">
+            <h3 class="display-4 head3">Education and Experiance</h3>
+        </div>
+    </div>
+    <div class="container">
+        <div class="col-sm-12">
+                <div class="card w-100">
+                    <div class="card-body">
+                        <div class="col-md-6">
+                            <h4 class="display-5">Summery</h4>
+                            <p class="card-text">In recent years, it has actually been rather effectively revealed that artficial knowledge systems can adequately create game content of a number of various types: textures, plants, degrees, maps, animals and so forth.</p>
+                            
+                            <h4 class="display-5"> Profesional Experiance</h4>
+                            <h4> <strong> Jr. Software Developer</strong></h4> <p>Codelances Software</p> <p>2018-2019</p> <p class="card-text">I had completed Intermediate From Gyan Udhyan Inter College</p>
+                            <h4><strong>Php Developer </strong></h4> <p>Techindia Software</p> <p>2022-Present</p> <p class="card-text">I had completed BCA From Dayanand Academy of Management Studies</p>
+                        
+                        </div>
+                        <div class="col-md-6">
+                            <h4 class="display-5">Education</h4>
+                            <h4> <strong> High School </strong></h4>2009-2010  <p>Gyan Udhyan Inter College</p><p class="card-text">I had completed High School From Gyan Udhyan Inter College</p>
+                            <h4> <strong> Intermediate</strong></h4>2011-2012  <p>Gyan Udhyan Inter College</p> <p class="card-text">I had completed Intermediate From Gyan Udhyan Inter College</p>
+                            <h4><strong> Bachlor of Computer Application</strong></h4>2012-2015  <p>Dayanand Academy of Management Studies</p> <p class="card-text">I had completed BCA From Dayanand Academy of Management Studies</p>
+                            <h4> <strong> Master in Computer Application</strong></h4>2015-2018  <p>Jagran Institute of Management</p> <p class="card-text">I had completed MCA From Jagran Institute of Management</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>       
+        </div>
+    </div>
+   <!-- Contact -->
+    <div class="container">
+        <div class="row d-flex p-2 bd-highlight"><h3 class="display-4 head3">Contact</h3></div>
+        <div class="col-sm-12">
+                <div class="card w-100 card-design text-center">
+                    <div class="card-body">
+                        <h5 class="display-5">Contact Information</h5>
+                        <sapn>Below you can see the necessary information if you like to contact to use.</span>
+                        <div class="icon-box"><i class="glyphicon glyphicon-earphone"></i></div> <div><h4>Phone:</h4> <p>+91-7905523576</p></div>
+                        <div class="icon-box"><i class="glyphicon glyphicon-envelope"></i></div> <div><h4>Email:</h4> <p>komalgautam01@gmail.com  | jim.komalgautam.mca@gmail.com</p></div>
+                        <div class="icon-box"><i class="fa-solid fa-location-dot"></i></div> <div><h4>Location:</h4> <p>Dada nagar Kanpur</p></div>
+                    </div>
+                    <div>
+                            <!-- Facebook -->
+                            <a class="text-right" href="#"><i class="fab fa-facebook-f"></i></a>
+                                
+
+                                <!-- Twitter -->
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+
+                                <!-- Google -->
+                                   <a href="#"><i class="fab fa-google"></i></a>
+
+                                <!-- Instagram -->
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+
+                                <!-- Linkedin -->
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+
+                                <!-- Pinterest -->
+                                <a href="#"><i class="fab fa-pinterest"></i></a>
+
+                                <!-- Vkontakte -->
+                                <a href="#"><i class="fab fa-vk"></i></a>
+
+                                <!-- Stack overflow -->
+                                <a href="#"> <i class="fab fa-stack-overflow"></i></a>
+
+                                <!-- Youtube -->
+                                <a href="#"><i class="fab fa-youtube"></i></a>
+
+                                <!-- Slack -->
+                                <a href="#"><i class="fab fa-slack-hash"></i></a>
+
+                                <!-- Github -->
+                                <a href="#"><i class="fab fa-github"></i></a>
+
+                                <!-- Dribbble -->
+                                <a href="#"><i class="fab fa-dribbble"></i></a>
+
+                                <!-- Reddit -->
+                                <a href="#"><i class="fab fa-reddit-alien"></i></a>
+
+                                <!-- Whatsapp -->
+                                <a href="#"><i class="fab fa-whatsapp"></i></a>
+                    </div>
+                </div>
+            </div>       
+        </div>
+    </div>
+    <div class="container-fluid">
+        <!-- Footer -->
+        <footer style="background: black;" class="page-footer font-small blue">
+                        
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© Copyright Designed By
+            <a href="#">Komal</a>
+        </div>
+        <!-- Copyright -->
+
+        </footer>
+        <!-- Footer -->
+    </div>
+ 
+
+</body>
+</html>
+</body>
+</html>
